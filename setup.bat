8e9b26b543c2f80f5793dae97298c1df979a2249
Anton Einarson <EINARSSO@kapsch.net>
2022-11-26 10:37:44 +0100

    update script and move of vimrc file

--- setup.bat
+++ setup.bat
@@ -1,11 +1,17 @@
-@echo off
+
 setlocal EnableDelayedExpansion 
 if "%1"=="" (  
-	echo Usage install.bat install update
+	echo Usage install.bat install pull setup
 ) 
 if "%1"=="install" (  
 	winget install --id Git.Git -e --source winget
 	winget install Microsoft.PowerToys --source winget
 	curl http://www.cygwin.com/setup-x86_64.exe -o .\cygwin_setup.exe
+)
+if "%1"=="pull" (
+	git clone https://github.com/Tofs/MySettings.git ./mysettings
+)
+if "%1"=="setup" (
+	cp ./.vimrc %home%/_vimrc
 )
 exit /b
