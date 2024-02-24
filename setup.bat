29401d7a0b5035e67a43bfb3ca14c3041eecebd8
Einarson Anton <anton.einarson@gmail.com>
2024-02-24 19:56:58 +0100

    Splittig

--- setup.bat
+++ setup.bat
@@ -1,18 +1,21 @@
 
 setlocal EnableDelayedExpansion 
-if "%1"=="" (  
-	echo Usage install.bat install pull setup
-) 
+if "%1"=="update" (
+	set INSTALL=1
+)
 if "%1"=="install" (  
+	set INSTALL=1
+	set REPOCLONE=1
+)
+if defined INSTALL (
 	winget install --id Git.Git -e --source winget
 	winget install Microsoft.PowerToys --source winget
 	winget install neovim
-  winget install BurntSushi.ripgrep.MSVC
+	winget install BurntSushi.ripgrep.MSVC
+  winget install python3
+	git config --global core.sshCommand "C:/Windows/System32/OpenSSH/ssh.exe"
 )
-if "%1"=="pull" (
-	cp -r %appdata%\..\Local\nvim .
-)
-if "%1"=="setup" (
-	cp ./.vimrc %home%/_vimrc
+if defined REPOCLONE (
+	git clone https://github.com/Tofs/neovim.git %appdata%\..\Local\nvim
 )
 exit /b
