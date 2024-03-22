0db3d789bd3765a08a07b55d1ccd6bbf42c8b6d5
Einarson Anton <anton.einarson@scania.com>
2024-03-22 07:37:11 +0100

    updates

--- setup.bat
+++ setup.bat
@@ -12,11 +12,14 @@ if defined INSTALL (
 	winget install Microsoft.PowerToys --source winget
 	winget install neovim
 	winget install BurntSushi.ripgrep.MSVC
-  winget install python3
-  winget install zip.zip
+	winget install python3
+	winget install zig.zig
+	winget install Agilebits.1Password
+  winget install OpenJS.NodeJS
 	git config --global core.sshCommand "C:/Windows/System32/OpenSSH/ssh.exe"
 )
 if defined REPOCLONE (
 	git clone https://github.com/Tofs/neovim.git %appdata%\..\Local\nvim
+	git clone https://github.com/ryanoasis/nerd-fonts
 )
 exit /b
