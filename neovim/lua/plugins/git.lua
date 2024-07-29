2ba80a0a565798ab9c78697cdcf0886fc6038e4b
Anton Einarson <anton.einarson@scania.com>
2024-07-29 08:30:23 +0200

    update git config

--- neovim/lua/plugins/git.lua
+++ neovim/lua/plugins/git.lua
@@ -1,10 +1,19 @@
 return {
-  {
-    "kdheepak/lazygit.nvim",
-    -- optional for floating window border decoration
-    dependencies = {
-      "nvim-lua/plenary.nvim",
+    {
+        "kdheepak/lazygit.nvim",
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        config = function()
+            vim.keymap.set("n", "gl", ":LazyGit\n", {})
+        end
     },
-    vim.keymap.set("n", "<C-g>", ":LazyGit\n", {})
-  }
+    {
+        "FabijanZulj/blame.nvim",
+        config = function()
+            require("blame").setup()
+            vim.keymap.set("n", "gb", ":BlameToggle\n", {})
+        end
+    }
 }
